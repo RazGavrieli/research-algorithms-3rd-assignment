edb76e5d6150d434883e91ea41e8b88e09a3eafa
Raz <razgavrieli@gmail.com>
2022-11-08 11:53:10 +0200

    email validation script is better, still needs sanity

--- q1/validEmail.py
+++ q1/validEmail.py
@@ -1,21 +1,45 @@
 import re
-
-# Make a regular expression
-# for validating an Email
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
  
-# Define a function for
-# for validating an Email
-def check(email):
- 
-    # pass the regular expression
-    # and the string into the fullmatch() method
-    if(re.fullmatch(regex, email)):
-        print("Valid Email")
- 
-    else:
-        print("Invalid Email")
+def validateEmail(email: str) -> bool:
+    """
+    Gets an email address (as a string) and determines if the email address is valid
+    >>> validateEmail("not_@valid.com")
+    False
+    >>> validateEmail("this_is@valid.com")
+    True
+    
+    
+    """
+    regex = r'\b[[a-zA-Z0-9]*[-._]?[a-zA-Z0-9]+]*@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.fullmatch(regex, email) != None
 
-check("not")
+def validateEmailsInFile(file: str) -> tuple:
+    regex = r'\b.*@.*\b'
+    valid = list()
+    invalid = list()
+    with open(file) as f:
+        for line in f:
+            for word in line.split():
+                if re.fullmatch(regex, word):
+                    if validateEmail(word):
+                        valid.append(word)
+                    else:
+                        invalid.append(word)
+    
+    return valid, invalid
+                    
 
-check("yes@yeso.com")
+
+if __name__ == "__main__":
+
+    import doctest
+    doctest.testmod()
+    print(validateEmail("not"))
+    print(validateEmail("yes@yeso.com"))
+    valid, invalid = validateEmailsInFile('emails.txt')
+    print('valids')
+    for i in valid:
+        print(i)
+    print('invalids')
+    for i in invalid:
+        print(i)
